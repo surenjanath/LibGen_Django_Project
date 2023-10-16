20fea7e9e300b583fb775b17e0a75a82858172c6
Surenjanath <surenjanath.singh@gmail.com>
2023-10-15 20:25:03 -0400

    Fixed download modal, deleted unused files

--- core/views.py
+++ core/views.py
@@ -4,8 +4,8 @@ from django.views import View
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
-import os
-from tempfile import NamedTemporaryFile
+from api.models import Search_Results
+from api.Functions import *
 
 class DashboardView(View):
     def get(self, request):
@@ -17,37 +17,23 @@ class DashboardView(View):
 
 @csrf_exempt
 def download_with_progress(request):
-    # Simulate a long-running download operation
-    download_url = "https://www.africau.edu/images/default/sample.pdf"  # Replace with the actual download URL
-    response = requests.get(download_url, stream=True)
+    unique_id = request.GET.get("id")
 
-    if response.status_code != 200:
-        return JsonResponse({'error': 'Unable to download file'})
+    session = requests.Session()
+    headers = {
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+            }
 
-    file_size = int(response.headers.get('content-length', 0))
-    progress = 0  # Initialize the progress variable
 
-    def generate_file_chunks():
-        progress = 0
-        with NamedTemporaryFile(delete=False) as temp_file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if not chunk:
-                    break
-                temp_file.write(chunk)
-                yield chunk
+    search_result = Search_Results.objects.get(uniqueId=unique_id)
+    links = GET_DOWNLOADLINK(session, search_result.md5_id, headers)  # Call your GET_DOWNLOADLINK function
+    print(links)
+    if links :
+        return  JsonResponse({
+        'search_result_title': search_result.title,
+        'download_links': links,
+        })
 
-                # Simulate progress update (you should replace this with actual progress tracking)
-                progress += len(chunk)
-                progress_percent = int((progress / file_size) * 100)
-                data = {'progress': progress_percent}
-                yield JsonResponse(data)
 
-        # After the download is complete, return an empty JSON response to signal completion
-        yield JsonResponse({})
+    return JsonResponse({'error': 'Unable to fetch links'})
 
-    # Set response headers
-    response = HttpResponse(generate_file_chunks(), content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="downloaded_file.pdf"'
-    response['Content-Length'] = file_size
-    print('Completed Download... ')
-    return response
